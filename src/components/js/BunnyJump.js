fc953c3c0900b639f39aa3ee22e9dbc16e5a8325
Dele <doyelese@gmail.com>
2023-02-14 09:49:20 -0700

    Fixed the bunny jump rendering

--- src/components/js/BunnyJump.js
+++ src/components/js/BunnyJump.js
@@ -1,57 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef }  from 'react';
 import Phaser from '../bunny-jump/src/lib/phaser';
 import Game from '../bunny-jump/src/scenes/Game.js'
 import GameOver from '../bunny-jump/src/scenes/GameOver.js'
 import '../css/Game.css'
 
 
-class BunnyJump extends React.Component {
 
-  componentDidMount() {
-    // Create a new instance of the Phaser.Game class
-    new Phaser.Game({
-        type: Phaser.AUTO,
-        width: 480,
-        height: 640,
-     
-         
-        physics: {
-            default: 'arcade',
-            arcade: {
-                gravity: {
-                    y: 200
-                },
-                debug: false
-            }
-        },
-        scene:[Game, GameOver]
+const BunnyJump = () => {
+  const gameContainer = useRef(null);
+
+  useEffect(() => {
+    const game = new Phaser.Game({
+      type: Phaser.AUTO,
+      width: 480,
+      height: 640,
+
+      
+      parent: gameContainer.current,
+
+      physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 200
+            },
+            debug: false
+        }
+    },
+    scene:[Game, GameOver]
     });
 
-  }
+    return () => {
+      game.destroy(true);
+    };
+  }, []);
 
-  shouldComponentUpdate() {
-    return false
-  }
-
-
-  // render() {
-  //   let comp;
-  //   if (this.state.display) {
-  //     comp = <BunnyJump />;
-  //   }
-  //   return (
-  //     <div>
-  //       {comp}
-  //       <button onClick={this.delete}>
-  //         Delete the component
-  //       </button>
-  //     </div>
-  //   );
-  // }
-  render() {
-    return <div classname='my-game' id="phaser-game" />;    
-  }
-
-}
+  return <div ref={gameContainer} />;
+};
 
 export default BunnyJump;
