2d22315878e816a98823af57d4283eefe5fd516b
Dele <doyelese@gmail.com>
2023-05-16 10:01:28 -0700

    fixed toastr on contact page

--- src/components/js/ContactForm.js
+++ src/components/js/ContactForm.js
@@ -1,56 +1,85 @@
-import { useRef,React } from 'react';
+import { useRef, React } from 'react';
 import '../css/ContactForm.css';
 import 'react-bootstrap';
 import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function ContactForm() {
-  const form =useRef()
-  
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_kcximed', 'template_fkiobg9', form.current, '-A3kGsVEqNXdIw9lR')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-      e.target.reset();
-  };
+  const form = useRef()
 
 
-  return (
-    
-    <form ref ={form} onSubmit = {sendEmail}>
-      <fieldset>
-        <legend>Contact Me</legend>
+  const CustomToast = () => {
+    return( 
+    <div className = "alert alert-dismissible alert-success" >
 
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1" className="form-label mt-4">Subject</label>
-          <input type="text" className="form-control" id="subject" name="subject" aria-describedby="contactPhone" placeholder="Enter your subject" />
-
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1" className="form-label mt-4">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" name="exampleInputEmail1" aria-describedby="contactEmail" placeholder="Enter email" />
-
-        </div>
-
-
-        <div className="form-group">
-          <label htmlFor="contactPhoneNumber" className="form-label mt-4">Phone Number</label>
-          <input type="text" className="form-control" id="contactPhoneNumber" name="contactPhoneNumber" aria-describedby="contactPhone" placeholder="Enter your phone number" />
-        </div>
- 
-        <div className="form-group">
-          <label htmlFor="exampleTextarea" className="form-label mt-4">Nature of Inquiry</label>
-          <textarea className="form-control" id="exampleTextarea" name="message" rows="3"></textarea>
-        </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-    </fieldset>
-  </form>
-          );
+    <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
+    <strong>Message sent successfully!</strong>  <a class= "alert-link" > I will reply as soon as possible</a>
+    </div>
+  )
 }
 
-          export default ContactForm;
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs.sendForm('service_kcximed', 'template_fkiobg9', form.current, '-A3kGsVEqNXdIw9lR')
+    .then((result) => {
+      console.log(result.text);
+    }, (error) => {
+      console.log(error.text);
+    });
+  console.log("TOAST")
+  toast.success(<CustomToast/>, {
+    position: toast.POSITION.TOP_RIGHT
+  });
+
+
+  e.target.reset();
+};
+
+const showToastMessage = () => {
+  console.log("TOAST")
+  toast.success('Success Notification !', {
+    position: toast.POSITION.TOP_RIGHT
+  });
+};
+
+return (
+
+  <form ref={form} onSubmit={sendEmail}>
+    <fieldset>
+      <legend>Contact Me</legend>
+
+      <div className="form-group">
+        <label htmlFor="exampleInputEmail1" className="form-label mt-4">Subject</label>
+        <input type="text" className="form-control" id="subject" name="subject" aria-describedby="contactPhone" placeholder="Enter your subject" />
+
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="exampleInputEmail1" className="form-label mt-4">Email address</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" name="exampleInputEmail1" aria-describedby="contactEmail" placeholder="Enter email" />
+
+      </div>
+
+
+      <div className="form-group">
+        <label htmlFor="contactPhoneNumber" className="form-label mt-4">Phone Number</label>
+        <input type="text" className="form-control" id="contactPhoneNumber" name="contactPhoneNumber" aria-describedby="contactPhone" placeholder="Enter your phone number" />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="exampleTextarea" className="form-label mt-4">Nature of Inquiry</label>
+        <textarea className="form-control" id="exampleTextarea" name="message" rows="3"></textarea>
+        <ToastContainer />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+
+    </fieldset>
+
+  </form>
+
+);
+}
+
+export default ContactForm;
--- src/components/pages/Tester.js
+++ src/components/pages/Tester.js
@@ -4,19 +4,40 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Tester = () => {
+
+    const CustomToast = ({ closeToast }) => {
+        return( 
+        <div className = "alert alert-dismissible alert-success" >
+    
+        <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
+        <strong>Well done!</strong> You successfully read  <i className="fa-duotone fa-face-smile-hearts"/><a  class= "alert-link" > this important alert message
+        </a>
+        <i className="fa-duotone fa-face-smile-hearts"></i>
+
+        </div>
+      )
+    }
+
     const showToastMessage = () => {
         console.log("TOAST")
+        toast.success(<CustomToast/>, {
+            position: toast.POSITION.TOP_RIGHT
+        });
+    };
+    const showToastMessage2 = () => {
+        console.log("TOAST2")
         toast.success('Success Notification !', {
-            position: toast.POSITION.BOTTOM_LEFT
+            position: toast.POSITION.TOP_RIGHT
         });
     };
 
     return (
    
         <div>
-        <button onClick={showToastMessage}>Notify</button>
+       
         <ToastContainer />
-        <button onClick={showToastMessage}>Notify2</button>
+        <button onClick={showToastMessage}>Notify</button>
+        <button onClick={showToastMessage2}>Notify2</button>
     </div>
     )
 }
